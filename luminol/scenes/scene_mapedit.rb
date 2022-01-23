025e07e3456a7d3b88818e02955f61d4bb8b702c
Matthew Lyons <matthew@nowaffles.com>
2022-01-23 13:39:55 -0800

    Outline widgets I would like to add

--- luminol/scenes/scene_mapedit.rb
+++ luminol/scenes/scene_mapedit.rb
@@ -13,73 +13,8 @@ class Scene_MapEdit
     skin = $system.skin
     icon.blt(0, 0, skin, Rect.new(192, 80, 16, 16))
 
-    @testwindow = Window_Draggable.new(0, 0, 480, 480, "Sound Test", icon)
-    @testwindow.contents = Bitmap.new(@testwindow.width, @testwindow.height)
-
-    @testwindow.on_draw :gradient, GradientInstruction.new(
-      rect: Rect.new(16, 16, 100, 24),
-      color1: Color.new(0, 0, 255, 255),
-      color2: Color.new(255, 0, 0, 255),
-    )
-    @testwindow.on_draw :font, FontInstruction.new(
-      name: "Cairo",
-      size: 24,
-      color: Color.new(255, 0, 200, 255),
-    )
-    @testwindow.on_draw :hello, TextInstruction.new(
-      text: "Hello!",
-      x: 16,
-      y: 16,
-    )
-    @testwindow.add_widget(
-      :button,
-      Button.new(Rect.new(48, 64, 64, 64), :normal)
-    )
-    button_proc = proc do |state|
-      @testwindow.on_draw :buttonfont, FontInstruction.new(
-        name: "Cairo",
-        size: 32,
-        color: Color.new(255, 255, 255),
-        align: 0,
-      )
-      if state
-        @testwindow.on_draw :buttontext, TextInstruction.new(text: "Pressed!", x: 48, y: 128)
-      else
-        @testwindow.on_draw :buttontext, TextInstruction.new(text: "Unpressed!", x: 48, y: 128)
-      end
-      @testwindow.draw
-    end
-    @testwindow.widget(:button).on_click(&button_proc)
-
-    # We can take advantage of the fact that adding an element to an array will not make a new object
-    radio_array = []
-    radio_array << RadioButton.new(Rect.new(48, 180, 64, 64), pressed: true, siblings: radio_array)
-    radio_array << RadioButton.new(Rect.new(128, 180, 64, 64), siblings: radio_array)
-    radio_array << RadioButton.new(Rect.new(208, 180, 64, 64), siblings: radio_array)
-    radio_array << RadioButton.new(Rect.new(288, 180, 64, 64), siblings: radio_array)
-    radio_proc = proc do |id|
-      @testwindow.on_draw :radiofont, FontInstruction.new(
-        name: "Terminus (TTF)",
-        size: 32,
-        color: Color.new(255, 255, 255),
-        align: 0,
-      )
-
-      @testwindow.on_draw :radiotext, TextInstruction.new(text: "Radio #{id} pressed", x: 48, y: 250)
-      @testwindow.draw
-    end
-    radio_array.each { |radio| radio.on_click(&radio_proc) }
-    radio_proc.call(0)
-
-    radio_array.each_with_index { |r, i| @testwindow.add_widget(eval(":radio_#{i}"), r) }
-
+    @testwindow = Window_Draggable.new(0, 0, 240, 480, "Sound Test", icon)
     @testwindow.draw
-
-    @testwindow2 = Window_Draggable.new(256, 0, 480, 480, "2 window test", icon)
-    @testwindow2.draw
-
-    @testwindow3 = Window_Draggable.new(512, 0, 480, 480, "3 window test", icon)
-    @testwindow3.draw
 
     loop do
       Graphics.update
@@ -101,7 +36,5 @@ class Scene_MapEdit
     $cursor.update
 
     @testwindow.update
-    @testwindow2.update
-    @testwindow3.update
   end
 end
