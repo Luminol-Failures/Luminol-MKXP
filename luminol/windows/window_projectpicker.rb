bf77ea948596ec841a45e762d5bc9e3384943669
Matthew Lyons <matthew@nowaffles.com>
2022-03-18 10:40:30 -0700

    Add blank windows for future

--- luminol/windows/window_projectpicker.rb
+++ luminol/windows/window_projectpicker.rb
@@ -19,7 +19,7 @@ class Window_ProjectPicker < Window_Draggable
 
     @picker.on_finish do |path, _|
       begin
-        PathCache.unmount($system.working_dir) # Unmount the current path
+        PathCache.unmount($system.working_dir, false) # Unmount the current path
       rescue Exception => e # rubocop:disable Lint/RescueException : We don't care if it fails or with what error
         STDERR.puts e.message
         STDERR.puts e.backtrace
